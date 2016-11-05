78f685a1cf7cddfa3f3828c8b406821bd6aabced
Kenge_NB <pgkusn@gmail.com>
2016-11-05 21:23:03 +0800

    update

--- BusinessCompany/js/script.js
+++ BusinessCompany/js/script.js
@@ -1,37 +1,31 @@
-$(function(){
-	$('.slide .inner').slick({
-		infinite: true,
-		speed: 500,
-		fade: true,
-		cssEase: 'linear',
-		autoplay: true,
-		prevArrow: '<button type="button" data-role="none" class="slick-prev">Previous</button>',
-		nextArrow: '<button type="button" data-role="none" class="slick-next">Next</button>',
-	});
+$('.msg').slick({
+	dots: true
+});
 
-	$('.msg').slick({
-		dots: true
-	});
+$('.content2 div.box').scrollex({
+	enter(){
+		$(this).addClass('fadeInRight');
+	}
+});
 
-	$(window).scroll(function(){
-		var top=$(document).scrollTop();
-		if(top>=183){
-			$('nav').css({
-				position:'fixed',
-				top:'0',
-				'z-index': '1000',
-				width: '100%',
-				'margin-top': '0'
-			});
-		}
-		else{
-			$('nav').css({
-				position:'static',
-				top:'auto',
-				'z-index': 'auto',
-				width: 'auto',
-				'margin-top': '35px'
-			});
-		}
-	});
+$(window).scroll(function(){
+	var top=$(document).scrollTop();
+	if(top>=183){
+		$('nav').css({
+			position:'fixed',
+			top:'0',
+			'z-index': '1000',
+			width: '100%',
+			'margin-top': '0'
+		});
+	}
+	else{
+		$('nav').css({
+			position:'static',
+			top:'auto',
+			'z-index': 'auto',
+			width: 'auto',
+			'margin-top': '35px'
+		});
+	}
 });
