ac8bb25ad518d0ed310c66ad03ecf71079ad3af2
Kenge <pgkusn@gmail.com>
2018-02-10 12:55:44 +0800

    CORS test

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 // Cross-Origin Resource Sharing Header
-header('Access-Control-Allow-Origin: http://base.com');
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept');
 
